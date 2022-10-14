ebf29fc770a90fb718e68c7b40562d2dcace0247
rosecoder0 <89976307+rosecoder0@users.noreply.github.com>
2022-10-14 23:20:44 +0300

    mekanbul

--- app_server/controllers/main.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-
-module.exports.index= function(req, res, next) { //anasayfayı kontrol eden fonksiyon
- res.render('index', { title: 'Express' });
-}
